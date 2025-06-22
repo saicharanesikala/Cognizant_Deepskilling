1d57331af6d07d95e7bbf78749f0fb34493533a3
saicharanesikala <saicharanesikala@gmail.com>
2025-06-22 09:11:49 +0530

    Update Search_linear_binary.java

--- Week-1_Data Structures and Algorithms/Exercise-2_E-commerce Platform Search/Code/Search_linear_binary.java
+++ Week-1_Data Structures and Algorithms/Exercise-2_E-commerce Platform Search/Code/Search_linear_binary.java
@@ -1,8 +1,30 @@
-
 import java.util.*;
 
 
 public class Search_linear_binary {
+	
+	
+	public static int bsearch(Product products[], int targetId) {
+		int l = 0;
+		int r = products.length;
+		int mid = 0;
+		int flag=0;
+		while(l <= r) {
+			mid = (l +r)/2;
+			if(products[mid].getProductId() == targetId) {
+				return mid;
+			}
+			else if(products[mid].getProductId() > targetId) {
+				//updating variables search size..
+				r= mid - 1;
+			}
+			else {
+				l = mid + 1;
+			}
+		}
+		return -1;
+	}
+	
 
 public static int lin_search(Product products[], int targetId) {
 		
@@ -13,29 +35,12 @@ public static int lin_search(Product products[], int targetId) {
 		}
 		return -1;
 		
-	}
+}
 	
-	public static int bsearch(Product products[], int targetId) {
-		int left = 0;
-		int right = products.length;
-		int mid = 0;
-		int flag=0;
-		while(left <= right) {
-			mid = (left + right)/2;
-			if(products[mid].getProductId() == targetId) {
-				return mid;
-			}
-			else if(products[mid].getProductId() < targetId) {
-				left = mid + 1;
-			}
-			else {
-				right = mid - 1;
-			}
-		}
-		return -1;
-	}
+	
 	
 	  public static void productid_sort(Product[] products) {
+		 
 		 Arrays.sort(products, Comparator.comparingInt(Product::getProductId));
 	}
 	
