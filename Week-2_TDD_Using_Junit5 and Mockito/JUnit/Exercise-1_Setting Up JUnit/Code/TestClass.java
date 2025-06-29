04981f4c858156cf7478ff798d825bcb77f71122
saicharanesikala <saicharanesikala@gmail.com>
2025-06-29 18:33:18 +0530

    Add files via upload

---
+++ Week-2_TDD_Using_Junit5 and Mockito/JUnit/Exercise-1_Setting Up JUnit/Code/TestClass.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestClass {
+    @Test
+    public void testAddition() {
+        int result = 20+ 30;
+        assertEquals(50, result);
+    }
+}
