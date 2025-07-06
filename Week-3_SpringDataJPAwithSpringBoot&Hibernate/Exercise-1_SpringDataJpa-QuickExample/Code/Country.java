f96f7ee0896f13e0dcbecfdd5ef768cc5eee304a
saicharanesikala <saicharanesikala@gmail.com>
2025-07-06 23:19:39 +0530

    Add files via upload

---
+++ Week-3_SpringDataJPAwithSpringBoot&Hibernate/Exercise-1_SpringDataJpa-QuickExample/Code/Country.java
@@ -0,0 +1,44 @@
+package com.cognizant.ormlearn.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    // ✅ Required Getters and Setters
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // ✅ Optional: For logging/debugging output
+    @Override
+    public String toString() {
+        return "Country{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
